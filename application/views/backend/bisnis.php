6fd3d661db7201e9f249957fdfdebf4985dd0a12
nenden <ndensme@gmail.com>
2022-08-19 14:41:32 +0700

    sfsf

--- application/views/backend/bisnis.php
+++ application/views/backend/bisnis.php
@@ -4,7 +4,7 @@
             <div class="message"></div>
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-themecolor"><i class="fa fa-braille" style="color:#1976d2"></i>&nbsp Dashboard Jprof</h3>
+                    <h3 class="text-themecolor"><i class="fa fa-braille" style="color:#1976d2"></i>&nbsp Dashboard Bisnis</h3>
                 </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
