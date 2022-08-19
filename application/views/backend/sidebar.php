9e9f9f7f7f11929cef02d33b95049d147c8314da
nenden <ndensme@gmail.com>
2022-08-19 14:49:17 +0700

    ssf

--- application/views/backend/sidebar.php
+++ application/views/backend/sidebar.php
@@ -17,27 +17,29 @@
                 <!-- End User profile text-->
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
-                    <?php if ($dep_id==2) { ?> 
+                   
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li> <a href="<?php echo base_url(); ?>" ><i class="mdi mdi-gauge"></i><span class="hide-menu">Website Rank </span></a></li>
-                        <!--<li> <a href="<?php echo base_url(); ?>media" ><i class="mdi mdi-instagram"></i><span class="hide-menu">Media Rank </span></a></li>!-->
-                        <li> <a href="<?php echo base_url(); ?>ga" ><i class="mdi mdi-gauge"></i><span class="hide-menu">GA Report </span></a></li>
-                        <li> <a href="<?php echo base_url(); ?>crm" ><i class="ti-wallet"></i><span class="hide-menu">CRM </span></a></li>  
+                        <?php if ($dep_id==2) { ?> 
+                            <li> <a href="<?php echo base_url(); ?>" ><i class="mdi mdi-gauge"></i><span class="hide-menu">Website Rank </span></a></li>
+                            <!--<li> <a href="<?php echo base_url(); ?>media" ><i class="mdi mdi-instagram"></i><span class="hide-menu">Media Rank </span></a></li>!-->
+                            <li> <a href="<?php echo base_url(); ?>ga" ><i class="mdi mdi-gauge"></i><span class="hide-menu">GA Report </span></a></li>
+                            <li> <a href="<?php echo base_url(); ?>crm" ><i class="ti-wallet"></i><span class="hide-menu">CRM </span></a></li>  
+                        <?php }?>
+                        <?php if ($dep_id==7) { ?>  
+                            <li class="nav-devider"></li>
+                            <li> <a href="<?php echo base_url(); ?>jprof" ><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard </span></a></li>
+                        <?php }?>
+                        <?php if ($dep_id==3) { ?> 
+                            
+                                <li class="nav-devider"></li>
+                                <li> <a href="<?php echo base_url(); ?>bisnis" ><i class="mdi mdi-gauge"></i><span class="hide-menu">CRM </span></a></li>
+                           
+                        <?php }?>
                     </ul> 
-                    <?php }?>
-                    <?php if ($dep_id==7) { ?> 
-                    <ul id="sidebarnav">
-                        <li class="nav-devider"></li>
-                        <li> <a href="<?php echo base_url(); ?>jprof" ><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard </span></a></li>
-                    </ul> 
-                    <?php }?>
-                    <?php if ($dep_id==3) { ?> 
-                    <ul id="sidebarnav">
-                        <li class="nav-devider"></li>
-                        <li> <a href="<?php echo base_url(); ?>bisnis" ><i class="mdi mdi-gauge"></i><span class="hide-menu">CRM </span></a></li>
-                    </ul> 
-                    <?php }?>
+                    
+                   
+                   
                     
                 </nav>
                 <!-- End Sidebar navigation -->
