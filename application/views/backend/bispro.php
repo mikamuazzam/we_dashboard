2e8e1e2aa30057483558c0d39bd89777110bc5bb
nenden <ndensme@gmail.com>
2023-02-13 07:31:57 +0700

    tes

--- application/views/backend/bispro.php
+++ application/views/backend/bispro.php
@@ -195,6 +195,6 @@
 
         </div>
        
-<script src="<?php echo base_url(); ?>assets/js/bispro1.js"></script>              
+<script src="<?php echo base_url(); ?>assets/js/bispro.js"></script>              
                                               
 <?php $this->load->view('backend/footer'); ?>
