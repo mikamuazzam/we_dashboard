5654e879a015847038e52226500ec4ba594a230a
nenden <ndensme@gmail.com>
2023-02-24 10:21:20 +0700

    MAP

--- application/views/backend/ga.php
+++ application/views/backend/ga.php
@@ -4,8 +4,8 @@
             <div class="message"></div>
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-themecolor"><i class="fa fa-braille" style="color:#1976d2"></i>
-                    &nbsp Google Analytics Report  &nbsp; &nbsp; <font color="black"> <?php echo date('d M Y');?></font></h3>
+                    <h4 class="text-themecolor"><i class="fa fa-braille" style="color:#1976d2"></i>
+                    &nbsp Google Analytics Report  &nbsp; &nbsp; <font color="black"> <?php echo date('d M Y');?></font></h4>
                 </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
@@ -16,7 +16,7 @@
                 </div>
              
             </div> 
-            <iframe width="100%" height="800" src="https://lookerstudio.google.com/embed/reporting/75a31d61-251d-4594-a491-2936234b0a28/page/1M" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe width="100%" height="800" src="https://lookerstudio.google.com/embed/reporting/4b5a4573-544f-4477-9901-7f2c045c2c9b/page/1M" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 
                
