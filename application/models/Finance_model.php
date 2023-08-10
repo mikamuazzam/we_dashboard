b511bb9f244fff8c4fb89808d285b8642fa191c9
mikamuazzam <mikamuazzam@gmail.com>
2023-08-10 09:04:36 +0700

    sfs

--- application/models/Finance_model.php
+++ application/models/Finance_model.php
@@ -18,7 +18,7 @@
 
             $sql="SELECT count(*)as jum,sum(a.based_value) as amount
                     from invoices a where status_data=1 
-                    and inv_status_id=$status $filter group by inv_status_id";
+                    and inv_status_id=$status and  year(created_at) >=2023  group by inv_status_id";
         
         $db2 = $this->load->database('db2', TRUE);
         $query=$db2->query($sql);
@@ -39,8 +39,7 @@
                     inner join inv_status c on c.id=a.inv_status_id 
                     inner join products d on d.id= a.product_id
                     inner join companies e on b.id_company = e. id
-                    where status_data=1  $filter and  ((inv_status_id = 1 and  a.created_at >='2022-10-31' ) or (inv_status_id > 1 and a.inv_date >='2022-10-31'))
-                LIMIT $limit OFFSET $offset ";
+                    where status_data=1   $filter and  year(a.created_at) >=2023";
         $db2 = $this->load->database('db2', TRUE);
         $query=$db2->query($sql);
       
@@ -53,7 +52,7 @@
         $sql = "SELECT count(*)as jum,sum(a.based_value) as amount,b.name 
                 from invoices a
                 inner join inv_status b on a.inv_status_id=b.id where status_data=1 
-                        and (inv_status_id=1 and a.created_at >='2022-10-31') or( a.inv_date >='2022-10-31') 
+                and  year(a.created_at) >=2023
                         group by b.name;";
         $db2 = $this->load->database('db2', TRUE);
         $query=$db2->query($sql);
@@ -78,7 +77,7 @@
     function total_po()
     {
         
-        $sql="SELECT count(*)as jumlah FROM deals where id_stage in(3,5,6,7) and created_at >'2022-09-30';";
+        $sql="SELECT count(*)as jumlah FROM deals where id_stage in(3,5,6,7) and  year(created_at) >=2023";
         
         $db2 = $this->load->database('db2', TRUE);
         $query=$db2->query($sql);
